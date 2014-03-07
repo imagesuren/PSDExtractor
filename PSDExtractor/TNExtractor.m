ebc86b2eb6277a77bd3ae88999684ce8a0065057
Eugene Tulushev <eugene@tulushev.com>
2014-03-07 17:38:33 +0400

    ~

--- PSDExtractor/TNExtractor.m
+++ PSDExtractor/TNExtractor.m
@@ -43,10 +43,6 @@ static NSString * const TNFontBounds = @"bounds";
 {
     PhotoshopCCApplication *photoshopApplication = [SBApplication applicationWithBundleIdentifier:TNPhotoshopBundleIdentifier];
     
-    PhotoshopCCE440 originalUnits = photoshopApplication.settings.rulerUnits;
-    photoshopApplication.settings.rulerUnits = PhotoshopCCE445PixelUnits;
-    photoshopApplication.settings.typeUnits = PhotoshopCCE450TraditionalSize;
-    
     PhotoshopCCDocument *currentDocument = photoshopApplication.currentDocument;
     
     self.documentFactor = 1.0f / currentDocument.height;
@@ -61,54 +57,43 @@ static NSString * const TNFontBounds = @"bounds";
         
         self.layersCount = currentDocumentLayers.count;
         
-        
-        
         self.count = 1;
         
         for (PhotoshopCCLayer *layer in currentDocumentLayers)
         {
             PhotoshopCCArtLayer *layerGet = [layer get];
             
-            PhotoshopCCE950 kind = layerGet.kind;
-            
-            NSLog(@"%li\tof\t%ld\t%@", (long)self.count, (long)self.layersCount, layerGet.name);
+            NSLog(@"%li\tof\t%ld", (long)self.count, (long)self.layersCount);
             
             NSArray *layerBounds = layerGet.bounds;
+
+            PhotoshopCCTextObject *textObject = layerGet.textObject;
             
-            if (kind == PhotoshopCCE950TextLayer && layerBounds && layerBounds.count == 4 && [layerBounds[2] integerValue] != 0)
-            {
-                PhotoshopCCTextObject *textObject = layerGet.textObject;
-                
-                NSString *fontName = textObject.font;
-                
-                NSNumber *size = @(textObject.size * self.documentFactor);
-                
-                NSNumber *x = @([layerBounds.firstObject doubleValue] * self.documentFactor);
-                
-                NSNumber *y = @([layerBounds[1] doubleValue] * self.documentFactor);
-                
-                NSNumber *width = @(([layerBounds[2] doubleValue] - [layerBounds.firstObject doubleValue]) * self.documentFactor);
-                
-                NSNumber *height = @(([layerBounds[3] doubleValue] - [layerBounds[1] doubleValue]) * self.documentFactor);
-                
-                NSString *contents = textObject.contents;
-                
-                NSDictionary *layerDictionary = @{@"font": @{@"name": fontName}, @"frame": @{@"x": x, @"y": y, @"width": width, @"height": height}, @"size": size, @"contents": contents};
-                
-                [self.layers addObject:layerDictionary];
-            }
+            NSString *fontName = textObject.font;
+            
+            NSNumber *size = @(textObject.size * self.documentFactor);
+            
+            NSNumber *x = @([layerBounds.firstObject doubleValue] * self.documentFactor);
+            
+            NSNumber *y = @([layerBounds[1] doubleValue] * self.documentFactor);
+            
+            NSNumber *width = @(([layerBounds[2] doubleValue] - [layerBounds.firstObject doubleValue]) * self.documentFactor);
+            
+            NSNumber *height = @(([layerBounds[3] doubleValue] - [layerBounds[1] doubleValue]) * self.documentFactor);
+            
+            NSString *contents = textObject.contents;
+            
+            NSDictionary *layerDictionary = @{@"font": @{@"name": fontName}, @"frame": @{@"x": x, @"y": y, @"width": width, @"height": height}, @"size": size, @"contents": contents};
+            
+            [self.layers addObject:layerDictionary];
             
             self.count++;
         }
-        
-        photoshopApplication.settings.rulerUnits = originalUnits;
         
         return @{@"document": @{@"name": documentName, @"width": documentWidth}, @"layers": [self.layers copy]};
     }
     else
     {
-        photoshopApplication.settings.rulerUnits = originalUnits;
-        
         return @{};
     }
 }
